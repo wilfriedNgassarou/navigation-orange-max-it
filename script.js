f465c8b6d0b6920329f15089be534e2197b6efef
Wilfried Ngassarou <125869257+wilfriedNgassarou@users.noreply.github.com>
2023-12-27 16:06:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Recupération de la liste d'options (30jours, 7jours, 24heures) et 
+// de la liste d'offres 
+const bundleList = document.querySelector('.bundle-container ul') ;
+const panorama = document.querySelector('.panorama') ;
+
+// Recupération de la largeur d'une balise article
+const bundleArticleComputedStyle = getComputedStyle(panorama.firstElementChild) ;
+const bundleArticleWidth = parseFloat(
+  bundleArticleComputedStyle.width 
+)
+
+bundleList.addEventListener('click', (e) => {
+  const index = e.target.closest('li').dataset.index ;
+
+  setActive(index, bundleList.children, panorama) ;
+})
+
+/**
+ *Gestion de la classe active sur l'option sélectionnée
+ * @param {Number} index 
+ * @param {NodeList} children 
+ * @param {HTMLElement} panorama 
+ */
+function setActive(index, children, panorama) {
+  const currentActive = document.querySelector('.active');
+  currentActive?.classList.remove('active') ;
+
+  children[index].classList.add('active') ;
+
+  panorama.style.transform = `translateX(-${index * bundleArticleWidth}px)` ;
+}
